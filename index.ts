9f5872245240deb2d803be273e57fa41f00b258e
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-09-18 01:00:41 -0300

    trigger lambda onObjectCreated

--- index.ts
+++ index.ts
@@ -8,5 +8,45 @@ const bucketname = config.require('bucketname');
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket(bucketname);
 
+// Configure IAM so that the AWS Lambda can be run.
+const lambdaDataLoaderHandlerRole = new aws.iam.Role(
+  'lambdaDataLoaderHandlerRole',
+  {
+    assumeRolePolicy: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'sts:AssumeRole',
+          Principal: {
+            Service: 'lambda.amazonaws.com',
+          },
+          Effect: 'Allow',
+          Sid: '',
+        },
+      ],
+    },
+  }
+);
+
+new aws.iam.RolePolicyAttachment('lambdaDataLoaderFuncRoleAttach', {
+  role: lambdaDataLoaderHandlerRole,
+  policyArn: aws.iam.ManagedPolicies.AWSLambdaFullAccess,
+});
+
+// Next, create the Lambda function itself:
+const lambdaDataLoaderHandlerFunc = new aws.lambda.Function(
+  'lambdaDataloader',
+  {
+    code: new pulumi.asset.AssetArchive({
+      '.': new pulumi.asset.FileArchive('./lambda-dataloader'),
+    }),
+    runtime: 'python3.8',
+    role: lambdaDataLoaderHandlerRole.arn,
+    handler: 'main.handler',
+  }
+);
+
+bucket.onObjectCreated('lambda-dataloader', lambdaDataLoaderHandlerFunc);
+
 // Export the name of the bucket
 export const bucketName = bucket.id;
