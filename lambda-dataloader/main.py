01fb5b07dd44f5570296df9befbe8670170af1fb
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-09-23 00:03:48 -0300

    init

--- lambda-dataloader/main.py
+++ lambda-dataloader/main.py
@@ -1,2 +1,16 @@
-def handler():
-  print('hello world!')
+import boto3
+import csv
+
+def handler(event, context):
+  s3_client = boto3.client('s3')
+  bucket = event['Records'][0]['s3']['bucket']['name']
+  filename = event['Records'][0]['s3']['object']['key']
+
+  download_path = f'/tmp/{filename}'
+  s3_client.download_file(bucket, filename, download_path)
+
+  with open(download_path) as file:
+    rows = csv.reader(file)
+    
+    for i, row in enumerate(rows):
+      print(i, row)
